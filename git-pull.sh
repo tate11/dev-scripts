876100cfeeb20bd67f25ac70a638d81c7e3e1d2a
jobiols <jobiols@users.noreply.github.com>
2015-02-14 15:06:35 -0300

    Update git-pull.sh

--- git-pull.sh
+++ git-pull.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
-# odoo v8
+# odoo V8  Trae todas las modificaciones desde los repositorios
 ##################################################################################
+#
 echo -e "\n******************************************* pull curso"
 cd /opt/odoo/custom/addons/curso
 sudo git pull
+#
 echo -e "\n******************************************* pull dev-scripts"
 cd /opt/odoo/dev-scripts
 sudo git pull
+#
 echo -e "\n******************************************* pull odoo-server"
 cd /opt/odoo/odoo-server
 sudo git pull
+#
 echo -e "\n******************************************* pull localizacion (ver nota)"
-# se rompio el pull desde bzr
 cd /opt/odoo/custom/addons/localizacion
 sudo bzr pull
+#
 echo -e "\n******************************************* pull odoerp_no_phoning_home"
 cd /opt/odoo/custom/addons/oerp_no_phoning_home
 sudo git pull
