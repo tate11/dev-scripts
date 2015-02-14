27e07fab53bab5c0ea804508263db7066f076975
jobiols <jobiols@users.noreply.github.com>
2015-02-14 07:49:41 -0300

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ###############################################################################
 # Script para correr los tests yaml
+# odoo v8
 #-------------------------------------------------------------------------------
 sudo -u odoo /opt/odoo/odoo-server/openerp-server -d test --addons-path=/opt/odoo/odoo-server/addons,/opt/odoo/custom/addons,/opt/odoo/custom/addons/localizacion --log-level=test -i curso --test-enable --stop-after-init
 exit
