14b77d9b2907e27a9cd337112a94a11888be0d73
jobiols <jobiols@users.noreply.github.com>
2015-02-14 07:46:15 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ################################################################################
 # Script para hrrancar openerp sin el log
+# odoo v8
 #-------------------------------------------------------------------------------
 sudo -u odoo /opt/odoo/odoo-server/openerp-server --addons-path=/opt/odoo/odoo-server/addons,/opt/odoo/custom/addons,/opt/odoo/custom/addons/localizacion --db_user=odoo --db_password=melquiades --log-level=debug 
 exit
