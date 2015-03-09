20fbaafc78583ef4a66fbacb8624c1540e6d5d5a
jobiols <jobiols@users.noreply.github.com>
2015-03-09 14:44:40 -0300

    Delete mail.update.sh

--- mail.update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-################################################################################
-# Script para hacer update al modulo test
-# odoo v8
-#-------------------------------------------------------------------------------
-sudo -u odoo /opt/odoo/odoo-server/openerp-server -d test --addons-path=/opt/odoo/odoo-server/addons,/opt/odoo/custom/addons,/opt/odoo/custom/addons/localizacion --update mail --stop-after-init
-exit
