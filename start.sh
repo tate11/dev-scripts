78ab68b2514fdb83e48a31570cf43f45b27700ca
root <jorge.obiols@gmail.com>
2015-03-17 00:42:01 +0000

    d

--- start.sh
+++ start.sh
@@ -3,13 +3,13 @@
 ###############################################
 sudo docker run \
   --name db -d --restart=always \
-  -e POSTGRES_PASSWORD=melquiades -e POSTGRES_USER=odoo \
+  -e POSTGRES_PASSWORD=odoo -e POSTGRES_USER=odoo \
   -v /home/ubuntu/80/data:/var/lib/postgresql/data \
    postgres:9.3.6
 
 sudo docker run -d --restart=always -p 80:8069 --link db:db \
   --name odoo \
-  jobiols/odoo:8.0
+  jobiols/odoo:80
 #  -v /home/ubuntu/80-80/log:/var/log/odoo \
 #  jobiols/odoo:80
 
