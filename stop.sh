cc99c8af903f19494202b925018805ec578cdb45
jobiols <jorge.obiols@gmail.com>
2015-03-28 17:17:00 -0300

    cosmetic

--- stop.sh
+++ stop.sh
@@ -3,7 +3,8 @@
 ###############################################
 sudo docker stop odoo
 sudo docker stop db
+sudo docker stop aeroo_docs
+
 sudo docker rm odoo
 sudo docker rm db
-sudo docker stop aeroo_docs
 sudo docker rm aeroo_docs
