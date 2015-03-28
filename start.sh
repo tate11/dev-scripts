7a1e48f9ff0f0407bbfca7df5a2ce2a8b78e1d88
jobiols <jorge.obiols@gmail.com>
2015-03-28 17:19:33 -0300

    bugfix

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 ###############################################
 sudo docker run -d --restart=always \
     --name db  \
-    -e POSTGRES_PASSWORD=odoo
+    -e POSTGRES_PASSWORD=odoo \
     -e POSTGRES_USER=odoo \
     -v /home/ubuntu/80/data:/var/lib/postgresql/data \
     postgres:9.3.6
@@ -12,7 +12,7 @@ sudo docker run -d -p 8989:8989 --restart=always \
     --name aeroo_docs  \
     rvalyi/aeroocker
 
-sudo docker run -d --restart=always -p 80:8069
+sudo docker run -d --restart=always -p 80:8069 \
     --name odoo \
     --link db:db \
     --link aeroo_docs:aeroo_docs \
