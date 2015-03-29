4a4b6712fb17e5c7a2acb2ff9f5f13d8691e891e
jobiols <jobiols@users.noreply.github.com>
2015-03-28 21:54:15 -0300

    Delete update-curso.sh

--- update-curso.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-################################################################################
-# Script para hacer update al modulo curso
-# odoo v8
-#-------------------------------------------------------------------------------
-sudo -u odoo /opt/odoo/odoo-server/openerp-server -d mk --addons-path=/opt/odoo/odoo-server/addons,/opt/odoo/custom/addons,/opt/odoo/custom/addons/localizacion --update curso --stop-after-init
-exit
