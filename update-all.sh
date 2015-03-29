5eb70fd85c3f87470c6b943b404cd5fb746a0206
jobiols <jobiols@users.noreply.github.com>
2015-03-28 21:53:45 -0300

    Delete update-all.sh

--- update-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-################################################################################
-# Script para hacer update all
-# odoo v8
-#-------------------------------------------------------------------------------
-sudo -u odoo /opt/odoo/odoo-server/openerp-server -d mk --addons-path=/opt/odoo/odoo-server/addons,/opt/odoo/custom/addons,/opt/odoo/custom/addons/localizacion --update all --stop-after-init
-exit
