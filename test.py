e1ff6a70d7bcb81d9776137992569c2c74f56257
jobiols <jorge.obiols@gmail.com>
2015-06-25 15:57:02 -0300

    fix port in console mode

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ import subprocess
 
 subprocess.call("ls -l", shell=True)
 
+target = raw_input("Enter an IP or Host to ping:\n")
+
+host = subprocess.Popen(['host', target], stdout = subprocess.PIPE).communicate()[0]
+
+print host
