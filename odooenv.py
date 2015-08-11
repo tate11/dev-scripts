35ffa07c8ad376f40f5f1ca1eab90e7e426e7b69
jobiols <jorge.obiols@gmail.com>
2015-08-10 21:34:19 -0300

    working at home

--- odooenv.py
+++ odooenv.py
@@ -186,10 +186,10 @@ def install_environment():
 
 def update_database():
 #    msgrun('Performing update database on ' + args.database[0] + ' with module ' + args.module[0]).
-    for d in args.database
+    for d in args.database:
         print d
 
-    for m in args.module
+    for m in args.module:
         print m
 
     return True
