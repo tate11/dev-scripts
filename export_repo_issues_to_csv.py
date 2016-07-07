b88d560f678ff687e1cd6e4bcc69181467beccdd
jobiols <jorge.obiols@gmail.com>
2016-07-07 15:42:10 -0300

    [WORKING] download issues from github

--- export_repo_issues_to_csv.py
+++ export_repo_issues_to_csv.py
@@ -4,43 +4,65 @@ Exports Issues from a specified repository to a CSV file
 Uses basic authentication (Github username + password) to retrieve Issues
 from a repository that username has access to. Supports Github API v3.
 """
-import csv
 import requests
 
 GITHUB_USER = 'jobiols'
-GITHUB_PASSWORD = '*Asterisco8'
-REPO = 'jobiols/valente'  # format is username/repo
+GITHUB_PASSWORD = ''
+REPO = 'odoo/odoo'  # format is username/repo
 ISSUES_FOR_REPO_URL = 'https://api.github.com/repos/%s/issues' % REPO
 AUTH = (GITHUB_USER, GITHUB_PASSWORD)
 
+nnn = 0
 
-def write_issues(r):
+
+def write_issues(nnn, r):
     "output a list of issues to csv"
     if not r.status_code == 200:
         raise Exception(r.status_code)
     for issue in r.json():
-        labels = issue['labels']
-        for label in labels:
-            if label['name'] == "Client Requested":
-                csvout.writerow([issue['number'], issue['title'].encode('utf-8'),
-                                 issue['body'].encode('utf-8'), issue['created_at'],
-                                 issue['updated_at']])
+        nnn += 1
+        print '#', nnn, issue['number'], issue['title']
+        print '-' * len(issue['title'])
+        if 'body' in issue:
+            print issue['body']
+            print '-' * len(issue['title'])
 
 
-r = requests.get(ISSUES_FOR_REPO_URL, auth=AUTH)
-csvfile = '%s-issues.csv' % (REPO.replace('/', '-'))
-csvout = csv.writer(open(csvfile, 'wb'))
-csvout.writerow(('id', 'Title', 'Body', 'Created At', 'Updated At'))
-write_issues(r)
+def get_pepe(ISSUES_FOR_REPO_URL, AUTH):
+    print 'get pepe'
+    return True
 
+
+def get_requests(ISSUES_FOR_REPO_URL, AUTH):
+    print 'get_requests'
+    if GITHUB_PASSWORD:
+        return requests.get(ISSUES_FOR_REPO_URL, auth=AUTH)
+    else:
+        return requests.get(ISSUES_FOR_REPO_URL)
+
+# csvfile = '%s-issues.csv' % (REPO.replace('/', '-'))
+# csvout = csv.writer(open(csvfile, 'wb'))
+# csvout.writerow(('id', 'Title', 'Body', 'Created At', 'Updated At'))
+
+r = get_requests(ISSUES_FOR_REPO_URL, AUTH)
+get_pepe()
+write_issues(nnn, r)
+print 'antes get pepe'
+
+print 'despues get'
 # more pages? examine the 'link' header returned
 if 'link' in r.headers:
     pages = dict(
         [(rel[6:-1], url[url.index('<') + 1:-1]) for url, rel in
          [link.split(';') for link in
           r.headers['link'].split(',')]])
+
     while 'last' in pages and 'next' in pages:
-        r = requests.get(pages['next'], auth=AUTH)
+        print 'antes get >>>>>', pages
+        get_pepe()
+        get_pepe(pages['next'], 1, auth=AUTH)
+        r = get_requests(pages['next'], 1, auth=AUTH)
+        print 'aaa', r
         write_issues(r)
         if pages['next'] == pages['last']:
             break
