e5cb9f890f92516002ae2ce1b6d7a66c347ec5ab
jobiols <jorge.obiols@gmail.com>
2016-10-30 16:25:47 -0300

    [ADD] update_fork se requiere soporte para multiples branches #12

--- update-forks.py
+++ update-forks.py
@@ -24,21 +24,28 @@ import argparse
 import sys
 from datetime import datetime
 import json
+import subprocess
 
 RED = "\033[1;31m"
 GREEN = "\033[1;32m"
 YELLOW = "\033[1;33m"
 YELLOW_LIGHT = "\033[33m"
 CLEAR = "\033[0;m"
-REPOS_DIR = '~/git-repos/'
+REPOS_DIR = '/home/jorge/git-repos/'
+
+
+def sc__(params):
+    if args.verbose:
+        print '>', params
+    p = subprocess.Popen(params.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return p.communicate()
 
 
 def sc_(params):
     if args.verbose:
         print '>', params
+    return subprocess.call(params, shell=True)
 
-
-# return subprocess.call(params, shell=True)
 
 def green(string):
     return GREEN + string + CLEAR
@@ -72,7 +79,8 @@ def msgerr(msg):
 def msginf(msg):
     print yellow_light(msg)
 
-# levantar datos de archivo de configuracion
+# levantar datos de archivo de configuracion, tiene
+# que estar en el mismo directorio que este archivo
 with open('update-forks-data.json') as f:
     json = json.load(f)
 repos = json['repos']
@@ -86,6 +94,29 @@ class repo:
         self._usr = dict['usr']
         self._repo = dict['repo']
         self._branch = dict['branch']
+        try:
+            self._upstream = dict['upstream']
+        except:
+            self._upstream = dict['repo']
+
+    def delete(self):
+        msgrun('deleting {}'.format(self.dir()))
+        sc_('sudo rm -r {}'.format(self.dir()))
+
+    def upstream(self):
+        return 'https://github.com/{}/{}'.format(self._usr, self._upstream)
+
+    def origin(self):
+        return 'https://github.com/jobiols/{}'.format(self._repo)
+
+    def usr(self):
+        return self._usr
+
+    def repo(self):
+        return self._repo
+
+    def branch(self):
+        return self._branch
 
     def format_branches(self, branches):
         if type(branches) == type([]):
@@ -100,6 +131,68 @@ class repo:
             self.format_branches(self._branch),
             self._repo)
 
+    def dir(self):
+        return REPOS_DIR + self._repo
+
+    def create_local_repo(self):
+        msgrun('Local repo does not exist! Cloning origin...')
+        sc_('git clone {} {}'.format(self.origin(), self.dir()))
+
+        msgrun('adding remote upstream')
+        sc_('git -C {} remote add upstream {}'.format(self.dir(), self.upstream()))
+
+    def _shortver(self):
+        return datetime.now().strftime('%Y%m%d%H%M')
+
+    def _longver(self):
+        return datetime.now().strftime(':%B %d, %Y')
+
+    def update(self):
+        msgrun('{} {}/{}:({})'.format(
+            20 * '-', self.usr(), self.repo(), ', '.join(self.branch())))
+
+        # chequear local repo y crearlo si no existe
+        if sc_('git -C {} status'.format(self.dir())):
+            self.create_local_repo()
+
+        # extraer con checkout todos los branches, si no existe crearlo
+        msgrun('checkin out branches ({})'.format(', '.join(self.branch())))
+        for branch in self.branch():
+            if sc_('git -C {} checkout {}'.format(self.dir(), branch)):
+                msgrun('Branch {} does not exist, creating it'.format(branch))
+                sc_('git -C {} checkout -b {}'.format(self.dir(), branch))
+
+        # traer los remotes
+        msgrun('Fetching all branches from upstream')
+        sc_('git -C {} fetch upstream'.format(self.dir()))
+
+        # mergear cada branch con el local
+        msgrun('Merging each remote branch with local ones')
+        for branch in self.branch():
+            sc_('git -C {} checkout {}'.format(self.dir(), branch))
+            sc_('git -C {} merge upstream/{}'.format(self.dir(), branch))
+
+        """
+        # le pongo el tag a la nueva versión creada
+        msgrun('tagging repo')
+        out, err = sc_('git -C {} tag -a v{} -m "update fork on {}"'.format(
+                self.dir(), self._shortver(), self._longver()))
+        print 'aaa',out,err
+        msgerr('tagging fail')
+        exit()
+        """
+        # subo la version nueva a mi repo, todos los branches
+        msgrun('pushing all branches to origin')
+        sc_('git -C {} push origin --all'.format(self.dir()))
+
+        """
+        # subo el tag
+        msgrun('push --tags')
+        if sc_('git -C {} push origin --tags {}'.format(
+                self.dir(), branch)):
+            msgerr('push fail')
+        """
+
 
 class repos:
     """ Conjunto de repositorios definido en el archivo de configuracion
@@ -111,6 +204,24 @@ class repos:
             if not self.alredy_exists(dict):
                 self._repos.append(repo(dict))
 
+    def find_repo(self, repo_name):
+        for repo in self._repos:
+            if repo.repo() == repo_name:
+                return repo
+        return False
+
+    def delete_local_repos(self):
+        if args.repo:
+            repo_name = args.repo[0]
+            repo = self.find_repo(repo_name)
+            if repo:
+                repo.delete()
+            else:
+                msgerr('no hay repo ' + repo_name)
+        else:
+            for repo in self._repos:
+                repo.delete()
+
     def alredy_exists(self, dict):
         for rp in self._repos:
             if rp._repo == dict['repo']:
@@ -120,6 +231,17 @@ class repos:
         for rp in self._repos:
             msginf(rp.name())
 
+    def update(self, repo_name):
+        for r in self._repos:
+            if r.repo() == repo_name:
+                r.update()
+                return True
+        msgerr('repo "{}" not found'.format(repo_name))
+
+    def update_all(self):
+        for r in self._repos:
+            r.update()
+
 
 rp = repos(json)
 
@@ -128,6 +250,26 @@ rp = repos(json)
 def list():
     msgrun('Currently maintained repositories')
     rp.list_repos()
+
+
+def update():
+    if args.repo is None:
+        msgerr('need -r')
+    rp.update(args.repo[0])
+
+
+def delete():
+    if args.repo is None:
+        msgerr('need -r')
+    rp.delete_local_repos()
+
+
+def delete_all():
+    rp.delete_local_repos()
+
+
+def update_all():
+    rp.update_all()
 
 
 if __name__ == '__main__':
@@ -148,13 +290,23 @@ if __name__ == '__main__':
     parser.add_argument('-u',
                         '--update',
                         action='store_true',
-                        help="Update specifics fork, use -b to specify branch")
+                        help="Update specifics fork, requires -r use -b to specify branch")
 
     parser.add_argument('-r',
                         '--repo',
                         action='append',
                         dest='repo',
                         help="Repo to update")
+
+    parser.add_argument('-d',
+                        '--delete',
+                        action='store_true',
+                        help="Delete specific repo, This is usefull to start fresh. Requires -r")
+
+    parser.add_argument('-D',
+                        '--delete-all',
+                        action='store_true',
+                        help="Delete all repos, This is usefull to start fresh.")
 
     parser.add_argument('-b',
                         '--branch',
@@ -183,128 +335,8 @@ if __name__ == '__main__':
     if args.update:
         update()
 
-exit()
-############################################################################################333
+    if args.delete:
+        delete()
 
-class repository:
-    def __init__(self, usr, repo, branch):
-        self._usr = usr
-        self._repo = repo
-        self._branch = branch
-
-    def usr(self):
-        return self._usr
-
-    def repo(self):
-        return self._repo
-
-    def branch(self):
-        return self._branch
-
-    def clone_str(self):
-        return 'git clone -b {} https://github.com/{}/{}'.format(
-            self._branch, self._usr, self._repo)
-
-
-class unit:
-    def __init__(self, dict):
-        try:
-            upstream_repo = dict['upstream']
-        except:
-            upstream_repo = dict['repo']
-
-        self._upstream = repository(dict['usr'], upstream_repo, dict['branch'])
-        self._origin = repository('jobiols', dict['repo'], dict['branch'])
-        self._local = REPOS_DIR + dict['repo']
-
-    def upstream(self):
-        return self._upstream
-
-    def origin(self):
-        return self._origin
-
-    def dir(self):
-        return self._local
-
-    def add_remote(self):
-        return 'git -C {} remote add upstream https://github.com/{}/{}'.format(
-            self._local, self._upstream.usr(), self._upstream.repo())
-
-
-def create_local_repo(rp):
-    msgrun('Local repo does not exist!')
-
-    msgrun('cloning origin')
-    if sc_(rp.origin().clone_str() + ' ' + rp.dir()):
-        msgerr('Fail!')
-
-    msgrun('adding remote upstream')
-    sc_(rp.add_remote())
-
-
-def update_repo(dict):
-    rp = unit(dict)
-    msgrun('{} {} {} {}'.format(
-        20 * '-', rp.upstream().usr(), rp.upstream().repo(), rp.upstream().branch()))
-
-    def _shortver():
-        return datetime.now().strftime('%Y%m%d%H%M')
-
-    def _longver():
-        return datetime.now().strftime(':%B %d, %Y')
-
-    # chequear local repo y crearlo si no existe
-    if sc_('git -C {} status'.format(rp.dir())):
-        create_local_repo(rp)
-
-    # traer el repo de upstream el repo original
-    msgrun('fetch upstream')
-    if sc_('git -C {} fetch upstream'.format(rp.dir())):
-        msgerr('fetch fail')
-
-    # lo extraigo
-    msgrun('checkout')
-    if sc_('git -C {} checkout {}'.format(
-            rp.dir(), rp.upstream().branch())):
-        msgerr('checkout fail')
-
-    # lo mergeo con el mio
-    msgrun('merge')
-    if sc_('git -C {} merge upstream/{}'.format(
-            rp.dir(), rp.upstream().branch())):
-        msgerr('merge fail')
-
-    # le pongo el tag a la nueva versión creada
-    msgrun('tagging repo')
-    if sc_('git -C {} tag -a v{} -m "update fork on {}"'.format(
-            rp.dir(), _shortver(), _longver())):
-        msgerr('tagging fail')
-
-    # subo la version nueva a mi repo
-    msgrun('push')
-    if sc_('git -C {} push origin {}'.format(
-            rp.dir(), rp.upstream().branch())):
-        msgerr('push fail')
-
-    # subo el tag
-    msgrun('push --tags')
-    if sc_('git -C {} push origin --tags {}'.format(
-            rp.dir(), rp.upstream().branch())):
-        msgerr('push fail')
-
-    msgdone('done')
-    return
-
-
-def update_all():
-    for repo in repos:
-        update_repo(repo)
-
-
-def update():
-    if args.repo is None:
-        msgerr('need -r')
-
-    for dict in repos:
-        if dict['repo'] == args.repo[0]:
-            update_repo(dict)
+    if args.delete_all:
+        delete_all()
