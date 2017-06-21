6d008a3aff57dc3c86aae067829909e64a4755b9
jobiols <jorge.obiols@gmail.com>
2017-06-21 10:50:09 -0300

    [fix] sigue mal un repo

--- classes/client_data.py
+++ classes/client_data.py
@@ -609,7 +609,8 @@ _clients = [
      ],
 
      'images': [
-         {'name': 'odoo', 'usr': 'jobiols', 'img': 'docker-openupgrade', 'ver': '9.0'},
+         {'name': 'aeroo', 'usr': 'jobiols', 'img': 'aeroo-docs'},
+         {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '9.0'},
          {'name': 'postgres', 'usr': 'postgres', 'ver': '9.4'},
          {'name': 'backup', 'usr': 'jobiols', 'img': 'backup'},
      ]
