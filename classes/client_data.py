410002843f883f1fcef1ae5eb0306267d905aac3
jobiols <jorge.obiols@gmail.com>
2017-09-19 17:44:45 -0300

    [ADD]  crear sams9

--- classes/client_data.py
+++ classes/client_data.py
@@ -518,37 +518,33 @@ _clients = [
     # ODOO V9
     #
     #######################################################################
-    {'name': 'suelos9', 'port': '8001', 'odoover': '9.0',
+    {'name': 'sams9', 'port': '8069', 'odoover': '9.0',
      'repos': [
-         {'usr': 'jobiols', 'repo': 'suelos', 'branch': '9.0'},
-
          {'usr': 'jobiols', 'repo': 'odoo-argentina', 'branch': '9.0'},
          {'usr': 'jobiols', 'repo': 'adhoc-aeroo_reports', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'adhoc-reporting-engine', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'server-tools', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'jeo', 'branch': '9.0'},
          {'usr': 'jobiols', 'repo': 'adhoc-account-payment', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-crm', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-reporting-engine', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-product', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-partner', 'branch': '9.0'},
          {'usr': 'jobiols', 'repo': 'adhoc-stock', 'branch': '9.0'},
          {'usr': 'jobiols', 'repo': 'adhoc-account-financial-tools', 'branch': '9.0'},
          {'usr': 'jobiols', 'repo': 'web', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'jeo', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'server-tools', 'branch': '9.0'},
-         {'usr': 'oca', 'repo': 'stock-logistics-workflow', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'journal-constraint', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-account-invoicing', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'crm', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'customer', 'branch': '9.0'},
 
-         # requeridos por la localizacion argentina -- 19/12/17
-         {'usr': 'jobiols', 'repo': 'adhoc-account-financial-tools', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'adhoc-miscellaneous', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'adhoc-account-payment', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'adhoc-aeroo_reports', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'adhoc-argentina-reporting', 'branch': '9.0'},
-         {'usr': 'jobiols', 'repo': 'adhoc-reporting-engine', 'branch': '9.0'},
      ],
      'images': [
          {'name': 'aeroo', 'usr': 'jobiols', 'img': 'aeroo-docs'},
          {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '9.0'},
          {'name': 'postgres', 'usr': 'postgres', 'ver': '9.4'},
+         {'name': 'backup', 'usr': 'jobiols', 'img': 'backup'},
      ]
      },
-    #######################################################################
+
     {'name': 'jeo9', 'port': '8010', 'odoover': '9.0',
      'repos': [
          # requeridos por la localizacion argentina -- 19/12/17
