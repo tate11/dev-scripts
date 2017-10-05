df4cb8374f805b1680a37d8935f56475855be44c
jobiols <jorge.obiols@gmail.com>
2017-10-05 17:31:05 -0300

    [ADD]  agregar glinsar

--- classes/client_data.py
+++ classes/client_data.py
@@ -518,6 +518,34 @@ _clients = [
     # ODOO V9
     #
     #######################################################################
+    {'name': 'glinsar', 'port': '8069', 'odoover': '9.0',
+     'repos': [
+         {'usr': 'jobiols', 'repo': 'cl-glinsar', 'branch': '9.0', 'host': 'bitbucket'},
+         {'usr': 'ingadhoc', 'repo': 'odoo-support', 'branch': '9.0'},
+
+         # requeridos por la localizacion argentina -- 19/12/17
+         {'usr': 'jobiols', 'repo': 'odoo-argentina', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-account-financial-tools', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-miscellaneous', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-account-payment', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-aeroo_reports', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-argentina-reporting', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-reporting-engine', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'adhoc-account-invoicing', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'partner-contact', 'branch': '9.0'},
+         {'usr': 'jobiols', 'repo': 'server-tools', 'branch': '9.0'},
+         {'usr': 'oca', 'repo': 'reporting-engine', 'branch': '9.0'},
+         {'usr': 'ingadhoc', 'repo': 'argentina-sale', 'branch': '9.0'},
+         {'usr': 'ingadhoc', 'repo': 'stock', 'branch': '9.0'},
+     ],
+     'images': [
+         {'name': 'aeroo', 'usr': 'jobiols', 'img': 'aeroo-docs'},
+         {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '9.0'},
+         {'name': 'postgres', 'usr': 'postgres', 'ver': '9.4'},
+#         {'name': 'backup', 'usr': 'jobiols', 'img': 'backup'},
+     ]
+     },
+
     {'name': 'sams9', 'port': '8069', 'odoover': '9.0',
      'repos': [
          {'usr': 'jobiols', 'repo': 'cl-sams', 'branch': '9.0', 'host': 'bitbucket'},
