61ebbf11b8cc9272dce2cd232c5f721d184be0b1
jobiols <jorge.obiols@gmail.com>
2017-12-24 11:05:30 -0300

    [FIX] change  version

--- odooenv.py
+++ odooenv.py
@@ -936,7 +936,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="""
         ==========================================================================
-        Odoo environment setup v6.0.0 by jeo Software <jorge.obiols@gmail.com>
+        Odoo environment setup v6.0.1 by jeo Software <jorge.obiols@gmail.com>
         With wdb support (https://github.com/Kozea/wdb) and Nginx (experimental)
         ==========================================================================
     """)
